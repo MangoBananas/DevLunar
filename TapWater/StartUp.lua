60a78557de9c2c68cc56153aa4b561df4e887b00
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-05 16:39:11 -0600

    Update StartUp.lua

--- TapWater/StartUp.lua
+++ TapWater/StartUp.lua
@@ -1,9 +1,9 @@
 	repeat task.wait() until game:IsLoaded()
---Loads The Game Identifier.
-	loadstring(game:HttpsGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/Tsunami/GameIdentifier.lua"))()
 --Display that the script has connected to the Roblox Client.
 	print ("Github Script Connected!")
 	game:GetService("StarterGui"):SetCore("SendNotification",{
 		Title = "Connection Stable",
 		Text = "Script has connected.\nFinding Game..."
 	})
+--Loads The Game Identifier.
+	loadstring(game:HttpsGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/Tsunami/GameIdentifier.lua"))()
