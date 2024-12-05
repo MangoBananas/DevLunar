372a1836bb8f081655c2dae3b86d6bcb3e3af2f7
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-05 17:33:04 -0600

    Update DragonAdventures.lua

--- DragonFire/DragonAdventures.lua
+++ DragonFire/DragonAdventures.lua
@@ -1,2 +1,12 @@
 -- Print in console showing that the script was found.
 print ("Dragon adventures script has been located and is running")
+  local MonSoonLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+  local MainWindow = MonSoonLib:MakeWindow({Name = "MonSoon Hub: Dragon Adventures", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+    local MainTab = MainWindow:MakeTab({
+	    Name = "Main",
+	    Icon = "rbxassetid://4483345998",
+	    PremiumOnly = false
+    })
+        local Section = MainTab:AddSection({
+	        Name = "Section 1"
+        })
