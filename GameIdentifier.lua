5d26fd17325c5bff4970062f903f53a5224644b8
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-05 17:15:02 -0600

    Update GameIdentifier.lua

--- GameIdentifier.lua
+++ GameIdentifier.lua
@@ -2,7 +2,6 @@
 -- This script is here to figure out if a script exist within the MonSoon Hub.
 
 --Testing
---THIS SHOULD BE REPLACED WITH A LOAD RAW GITHUB STRING
 if game.PlaceId == 6590224210 then
 	print ("Game ID: TEST")
 	print (game.PlaceId)
@@ -10,8 +9,9 @@ if game.PlaceId == 6590224210 then
 		Title = "Script Found",
 		Text = "Opening MonSoonHub..."
 	})
+--This should be changes as this should be the test script.
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/TropicalStorms/DragonFire/DragonAdventures.lua"))()
 	--Dragon Adventures:
-	    --THIS SHOULD BE REPLACED WITH A LOAD RAW GITHUB STRING
     elseif game.PlaceId == 3475397644 then
 	    print ("Game ID: Dragon Adventures")
 	    print (game.PlaceId)
@@ -19,6 +19,7 @@ if game.PlaceId == 6590224210 then
 		    Title = "Script Found",
 		    Text = "Opening MonSoonHub..."
 	    })
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/TropicalStorms/DragonFire/DragonAdventures.lua"))()
 	--Game is not in the system:
 else
 	task.wait(5)
