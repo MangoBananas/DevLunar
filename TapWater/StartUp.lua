e5cec9a3709510342a7d4bdc60b8a4c585920ad9
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-05 16:51:15 -0600

    Update StartUp.lua

--- TapWater/StartUp.lua
+++ TapWater/StartUp.lua
@@ -5,6 +5,11 @@
 		Title = "Connection Stable",
 		Text = "Script has connected.\nFinding Game..."
 	})
+	task.wait(1)
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = "Loading...",
+		Text = "Please do not re-execute."
+	})
 	task.wait(5)
 --Loads The Game Identifier.
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/Tsunami/GameIdentifier.lua"))()
