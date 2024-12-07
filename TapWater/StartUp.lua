a8b2e7cb9fd5719dc82e3622274845156c979d00
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-07 17:17:27 -0600

    Update StartUp.lua

--- TapWater/StartUp.lua
+++ TapWater/StartUp.lua
@@ -1,15 +1,9 @@
 	repeat task.wait() until game:IsLoaded()
 --Display that the script has connected to the Roblox Client.
-	print ("Github Script Connected!")
+	print ("Start Up Script Connected!")
 	game:GetService("StarterGui"):SetCore("SendNotification",{
 		Title = "Connection Stable",
 		Text = "Script has connected.\nFinding Game..."
-	})
-	task.wait(1)
-	print ("Identifing Game. Do nor re-execute")
-	game:GetService("StarterGui"):SetCore("SendNotification",{
-		Title = "Loading...",
-		Text = "Please do not re-execute."
 	})
 	task.wait(5)
 --Loads The Game Identifier.
