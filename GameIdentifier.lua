a7ad530dd71298ac93a417353967ead3894d1435
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-07 17:19:53 -0600

    Update GameIdentifier.lua

--- GameIdentifier.lua
+++ GameIdentifier.lua
@@ -7,7 +7,7 @@ if game.PlaceId == 6590224210 then
 	print (game.PlaceId)
 	game:GetService("StarterGui"):SetCore("SendNotification",{
 		Title = "Script Found",
-		Text = MarketplaceService:GetProductInfo(game.PlaceId).Name
+		Text = .. MarketplaceService:GetProductInfo(game.PlaceId).Name
 	})
 --This should be changes as this should be the test script.
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/MangoBananas/DevLunar/refs/heads/TropicalStorms/DragonFire/DragonAdventures.lua"))()
