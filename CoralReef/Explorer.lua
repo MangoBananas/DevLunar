3ea49b24013a115df60870a325912a21f2808bb2
MangoBananas <mfnaf84gamer@gmail.com>
2024-12-07 12:42:27 -0600

    Create Explorer.lua

---
+++ CoralReef/Explorer.lua
@@ -0,0 +1,17 @@
+--Resources
+Workspace.Interactions.Nodes.Resources.LargeResourceNode.Hitbox
+--Food
+Workspace.Interactions.Nodes.Food.LargeFoodNode.Hitbox
+-- ? ReplicatedStorage.Storage.Nodes.Items.Food:GetChildren()
+
+--Bonemeal
+Workspace.Interactions.Nodes.BoneMeal.HitBox
+--Egg Model
+Workspace.Interactions.Nodes.Eggs.ActiveNodes.Lobby.EggModel.Egg
+--Egg Harvest
+HarvestGui
+--Farm Plot
+Workspace.Interaction.FarmPlot.InteractionParts.ActivePlants
+
+--Time Trials
+ReplicatedStorage.WorldStorage.TimeTrialCourses.
